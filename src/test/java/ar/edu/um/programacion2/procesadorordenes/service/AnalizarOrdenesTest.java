b7683564baff4368626e710bf22bacc405ded54d
militomba <mariamilagrostomba@gmail.com>
2023-12-21 10:58:50 -0300

    trabajo listo

--- src/test/java/ar/edu/um/programacion2/procesadorordenes/service/AnalizarOrdenesTest.java
+++ src/test/java/ar/edu/um/programacion2/procesadorordenes/service/AnalizarOrdenesTest.java
@@ -1,13 +1,18 @@
 package ar.edu.um.programacion2.procesadorordenes.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application.yml")
 public class AnalizarOrdenesTest {
 
     @InjectMocks
@@ -30,16 +35,25 @@ public class AnalizarOrdenesTest {
     public void clienteInvalidoTest() {
         AnalizarOrdenes clienteMock = mock(AnalizarOrdenes.class);
         when(clienteMock.obtenerClientesServicioProfe(2)).thenReturn(false);
-        boolean resultado = clienteMock.obtenerClientesServicioProfe(1150);
+        boolean resultado = clienteMock.obtenerClientesServicioProfe(2);
         verify(clienteMock).obtenerClientesServicioProfe(2);
         assertFalse(resultado);
     }
 
     @Test
     public void codigoIdValidoTest() {
-        AnalizarOrdenes clienteMock = mock(AnalizarOrdenes.class);
-        when(clienteMock.obtenerAccionesServicioProfe(13)).thenReturn(true);
-        assertTrue(clienteMock.obtenerAccionesServicioProfe(13));
-        verify(clienteMock).obtenerAccionesServicioProfe(13);
+        AnalizarOrdenes codigoMock = mock(AnalizarOrdenes.class);
+        when(codigoMock.obtenerAccionesServicioProfe(13)).thenReturn(true);
+        assertTrue(codigoMock.obtenerAccionesServicioProfe(13));
+        verify(codigoMock).obtenerAccionesServicioProfe(13);
+    }
+
+    @Test
+    public void codigoIdInvalidoTest() {
+        AnalizarOrdenes codigoMock = mock(AnalizarOrdenes.class);
+        when(codigoMock.obtenerAccionesServicioProfe(555)).thenReturn(false);
+        boolean resultado = codigoMock.obtenerAccionesServicioProfe(555);
+        verify(codigoMock).obtenerAccionesServicioProfe(555);
+        assertFalse(resultado);
     }
 }
